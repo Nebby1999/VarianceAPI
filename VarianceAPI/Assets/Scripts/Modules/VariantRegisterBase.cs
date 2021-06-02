01d72d9aace7455c20881b5f0ac8b53497bf4301
Nebby <nebby131999@gmail.com>
2021-06-02 15:44:34 -0400

    Removed Commented out code

--- VarianceAPI/Assets/Scripts/Modules/VariantRegisterBase.cs
+++ VarianceAPI/Assets/Scripts/Modules/VariantRegisterBase.cs
@@ -14,59 +14,20 @@ namespace VarianceAPI.Modules
             assetBundle = assets;
             if(assetBundle == null)
             {
-                Debug.LogError("AssetBundle is null!");
+                Debug.LogError("VarianceAPI: AssetBundle is null! Aborting");
                 return;
             }
             variantInfos = assetBundle.LoadAllAssets<VariantInfo>();
             if(variantInfos == null)
             {
-                Debug.LogError("VariantInfo array is empty!");
+                Debug.LogError("VarianceAPI: VariantInfo array is empty! Aborting");
                 return;
             }
             foreach(VariantInfo i in variantInfos)
             {
-                Debug.Log("Adding " + i.identifierName + "VariantHandler for the bodyPrefab of name " + i.bodyName + "Body!");
+                Debug.Log("VarianceAPI: Adding " + i.identifierName + " VariantHandler for the bodyPrefab of name " + i.bodyName + "Body!");
                 Helpers.AddVariant(i);
             }
         }
     }
-    /*public abstract class VariantRegisterBase<T> : VariantRegisterBase where T : VariantRegisterBase<T>
-    {
-        public static T instance { get; private set; }
-    }
-    public abstract class VariantRegisterBase
-    {
-        public abstract AssetBundle AssetBundle { get; }
-        protected List<VariantInfo> variantList;
-        public abstract void Register();
-        protected void GetVariantInfos()
-        {
-            if (AssetBundle == null)
-            {
-                Debug.LogError("Asset Bundle is null! Aborting!");
-                return;
-            }
-            var variantInfos = AssetBundle.LoadAllAssets<VariantInfo>();
-            foreach (VariantInfo varInfo in variantInfos)
-            {
-                variantList.Add(varInfo);
-            }
-        }
-        protected void RegisterVariantInfos()
-        {
-            if(variantList == null)
-            {
-                Debug.LogError("Variant List is null! aborting the creation of variants from " + AssetBundle.name);
-            }
-            foreach (VariantInfo variant in variantList)
-            {
-                if(variant == null)
-                {
-                    Debug.LogError("Variant info is null! Aborting!");
-                    continue;
-                }
-                Helpers.AddVariant(variant);
-            }
-        }
-    }*/
 }
