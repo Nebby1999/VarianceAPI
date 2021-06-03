986a707dd544296f979915c65a693f96d4f3fc35
Nebby <nebby131999@gmail.com>
2021-06-03 19:26:40 -0400

    Added VariantRegister class

---
+++ ExampleVariantPack/Assets/Scripts/VariantRegister.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using VarianceAPI.Modules;
+using VarianceAPI.Scriptables;
+
+namespace ExampleVariantPack.ThunderkitMadeVariants
+{
+    public class VariantRegister : VariantInfoHandler
+    {
+        public void RegisterConfigs(AssetBundle assets, ConfigFile config)
+        {
+            Init(assets, config);
+        }
+        public void RegisterCodedVariantConfigs(List<VariantInfo> variantInfos, ConfigFile config)
+        {
+            Init(variantInfos, config);
+        }
+    }
+}
