bb4def3ec72f2a4cadefa660243d157dc385617b
Nebby <nebby131999@gmail.com>
2021-06-03 15:14:55 -0400

    Added VariantConfig scriptable object

---
+++ VarianceAPI/Assets/Scripts/Scriptables/VariantConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace VarianceAPI.Scriptables
+{
+    [CreateAssetMenu(fileName = "VariantConfig", menuName = "VarianceAPI/VariantConfig", order = 1)]
+    public class VariantConfig : ScriptableObject
+    {
+        public float spawnRate;
+        public bool isUnique;
+        public string identifier;
+
+        public bool isModded;
+        public string modAuthor;
+        public string modName;
+    }
+}
