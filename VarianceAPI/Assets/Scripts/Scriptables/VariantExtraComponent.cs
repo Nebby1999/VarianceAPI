c2cb867a066e11af029123c819566f3291f89491
Nebby <nebby131999@gmail.com>
2021-06-05 19:32:31 -0400

    Added a new VariantExtraComponent scriptable object, will be used to add custom Components to variants.

---
+++ VarianceAPI/Assets/Scripts/Scriptables/VariantExtraComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace VarianceAPI.Scriptables
+{
+    [CreateAssetMenu(fileName = "VariantExtraComponent", menuName = "VarianceAPI/VariantExtraComponent", order = 11)]
+    public class VariantExtraComponent : ScriptableObject
+    {
+        [Header("Variant Extra Component")]
+            [Tooltip("What component to add to the Variant")]
+            public UnityEngine.Object componentToAdd;
+
+            [Tooltip("Wether the component is Aesthetic, such as adding a missile launcher to an enemy")]
+            public bool isAesthetic;
+    }
+}
