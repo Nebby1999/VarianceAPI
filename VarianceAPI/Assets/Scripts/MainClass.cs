6f728e2ac2f1a17b6a1e9ace77eb2a49e31b79a2
Nebby <nebby131999@gmail.com>
2021-06-06 21:02:31 -0400

    Implemented Artifact of Variance

--- VarianceAPI/Assets/Scripts/MainClass.cs
+++ VarianceAPI/Assets/Scripts/MainClass.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using RoR2;
 using RoR2.ContentManagement;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ using System.Security;
 using System.Security.Permissions;
 using UnityEngine;
 using VarianceAPI.Modules;
+using Path = System.IO.Path;
 
 [module: UnverifiableCode]
 #pragma warning disable CS0618 // Type or member is obsolete
@@ -26,13 +28,35 @@ namespace VarianceAPI
         {
             instance = this;
 			ConfigLoader.SetupConfigLoader(Config);
-            //LoadAssetsAndRegisterContentPack();
+            LoadAssetsAndRegisterContentPack();
         }
         internal void LoadAssetsAndRegisterContentPack()
         {
             var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             varianceAPIAssets = AssetBundle.LoadFromFile(Path.Combine(path, assetBundleName));
-            ContentPackProvider.serializedContentPack = varianceAPIAssets.LoadAsset<SerializableContentPack>(ContentPackProvider.contentPackName);
+			FinishArtifactOfVariance();
+			ContentPackProvider.serializedContentPack = varianceAPIAssets.LoadAsset<SerializableContentPack>(ContentPackProvider.contentPackName);
+			ContentPackProvider.Initialize();
+        }
+		internal void FinishArtifactOfVariance()
+        {
+			var VarianceDef = varianceAPIAssets.LoadAsset<ArtifactDef>("VarianceDef");
+			var VarianceContent = varianceAPIAssets.LoadAsset<SerializableContentPack>(ContentPackProvider.contentPackName);
+			if(ConfigLoader.EnableArtifactOfVariance.Value)
+            {
+				Debug.Log("VarianceAPI: Registering Artifact Of Variance...");
+				ArtifactDef[] ArtifactDefs = new ArtifactDef[] { VarianceDef };
+				VarianceDef.descriptionToken = "All Variant's Spawn Rates are Multiplied by " + ConfigLoader.VarianceMultiplier.Value;
+				VarianceContent.artifactDefs = ArtifactDefs;
+				if(VarianceContent.artifactDefs != null)
+                {
+					Debug.Log("VarianceAPI: Succesfully added " + VarianceDef.nameToken + " to " + ContentPackProvider.contentPackName);
+                }
+            }
+			else
+            {
+				return;
+            }
         }
 	}
 	public class ContentPackProvider : IContentPackProvider
@@ -40,7 +64,7 @@ namespace VarianceAPI
 		public static SerializableContentPack serializedContentPack;
 		public static ContentPack contentPack;
 		//Should be the same names as your SerializableContentPack in the asset bundle
-		public static string contentPackName = "VarianceAPIAssets";
+		public static string contentPackName = "VarianceAPIContent";
 
 		public string identifier
 		{
@@ -53,6 +77,7 @@ namespace VarianceAPI
 
 		internal static void Initialize()
 		{
+			Debug.Log("VarianceAPI: Registering VarianceAPIContent...");
 			contentPack = serializedContentPack.CreateContentPack();
 			ContentManager.collectContentPackProviders += AddCustomContent;
 		}
