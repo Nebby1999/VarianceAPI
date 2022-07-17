51690a82d5b81e464d9229c1873f0d0c38146fa8
Nebby <nebby131999@gmail.com>
2022-07-16 20:53:11 -0400

    Forgor

--- Runtime/Code/VAPIUtils.cs
+++ Runtime/Code/VAPIUtils.cs
@@ -10,11 +10,6 @@ namespace VAPI
 {
     public static class VAPIUtils
     {
-        public static int GetItemCount(this CharacterBody body, ItemDef itemDef)
-        {
-            return body.inventory == null ? 0 : body.inventory.GetItemCount(itemDef);
-        }
-
         public static void WriteVariantIndex(this NetworkWriter writer, VariantIndex index)
         {
             writer.Write((int)index);
